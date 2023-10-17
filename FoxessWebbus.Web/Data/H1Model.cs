635e94a68368b806ad250e5d96ac4a6cf7f9fb53
obiwanconobi <connerpanaro@gmail.com>
2023-10-17 15:29:46 +0100

    most values added
    added most values.
    some minor formatting
    new methods for formatting data etc

--- FoxessWebbus.Web/Data/H1Model.cs
+++ FoxessWebbus.Web/Data/H1Model.cs
@@ -5,10 +5,14 @@ public class H1Model
     public short PVPower1 {get;set;}
     public short PVPower2 {get;set;}
     public short PVPowerTotal {get; set;} 
-    public short BatteryChargeRate { get; set; }
+    public short BatteryCharge { get; set; }
+    public short BatteryDischarge {get;set;}
     public short BatterySoc { get; set; }
     public short BatteryTemp { get; set; }
     public short InverterTemp { get; set; }
+    public short FeedIn {get;set;}
+    public short FromGrid {get;set;}
+
 
   
     public DateTime LoggedDateTime { get; set; }
