2349532c7128c026c469b98db4fd1799aa148a9d
obiwanconobi <connerpanaro@gmail.com>
2023-10-19 22:56:36 +0100

    app settings reader
    appsettings reader and writer
    basic energy graph poc

--- FoxessWebbus.Web/Program.cs
+++ FoxessWebbus.Web/Program.cs
@@ -3,13 +3,16 @@ using Microsoft.AspNetCore.Components.Web;
 using FoxessWebbus.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-
+using MudBlazor.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
